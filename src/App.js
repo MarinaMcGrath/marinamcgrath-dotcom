e85a14823aa3151ada3349263f08742adc129adb
MarinaMcGrath <marinamcgrath92@gmail.com>
2021-11-22 18:54:24 -0800

    base app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Marina McGrath
-        </p>
-        <p>
-          <a href='https://github.com/marinamcgrath'>Github</a> | 
-          <a href='https://www.linkedin.com/in/marinamcgrath'> LinkedIn</a> |
-          <a href='https://medium.com/@marinamcgrath'> Medium</a> |
-          <a href='/resume'> Resume</a>
-        </p>
+        <div className="Hero">
+          <p className="name">
+            Marina McGrath
+          </p>
+          <p id="icons">
+          <a href="https://github.com/MarinaMcGrath"  target="_blank">
+            <img src="/img/GitHub.png" alt="github"  className="icon"/>
+          </a>
+          <a href="https://www.linkedin.com/in/marinamcgrath"  target="_blank">
+            <img src="/img/LI.png" alt="linkedin"  className="icon"/>
+          </a>
+          <a href="https://medium.com/@marinamcgrath"  target="_blank">
+            <img src="/img/medium.png" alt="medium"  className="icon"/>
+          </a>
+            <a href='/resume'>
+            <img src="/img/resume.png" alt="resume"  className="icon"/>
+            </a>
+          </p>
+        </div>
       </header>
     </div>
   );
